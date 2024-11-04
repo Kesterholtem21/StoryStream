ca0dc0fc487e199ed9a81edbc768c3a06fae5778
josephshin22 <shinjj22@gcc.edu>
2024-11-04 15:30:23 -0500

    Add movie db class

--- app.py
+++ app.py
@@ -4,11 +4,12 @@ from flask_sqlalchemy import SQLAlchemy
 
 # make sure the script's directory is in Python's import path
 # this is only required when run from a different directory
-import os, sys
+import os
+import sys
 script_dir = os.path.abspath(os.path.dirname(__file__))
 sys.path.append(script_dir)
 # once script directory is in path, import Contact and ContactForm
-#from library_forms import BookForm, AuthorForm
+# from library_forms import BookForm, AuthorForm
 
 scriptdir = os.path.dirname(os.path.abspath(__file__))
 dbfile = os.path.join(scriptdir, "library.sqlite3")
@@ -17,40 +18,48 @@ dbfile = os.path.join(scriptdir, "library.sqlite3")
 app = Flask(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 app.config['SECRET_KEY'] = 'correcthorsebatterystaple'
-# TODO: connect your app to a SQLite database
-
-
-
 app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{dbfile}"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-
 db = SQLAlchemy(app)
 
+# Database model for IMDB movies
+
+
+class Movie(db.Model):
+    __tablename__ = 'Movies'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    genre = db.Column(db.String, nullable=False)
+    age_rating = db.Column(db.String, nullable=False)
+    imdb_rating = db.Column(db.String, nullable=False)
 
 
 @app.route("/register/")
 def get_register():
-    #TODO create register GET route
+    # TODO create register GET route
     pass
+
 
 @app.route("/register/")
 def post_register():
-    #TODO create register POST route
+    # TODO create register POST route
     pass
+
 
 @app.route("/login/")
 def get_login():
-    #TODO create login GET route
+    # TODO create login GET route
     pass
+
 
 @app.route("/login/")
 def post_login():
-    #TODO create login POST route
+    # TODO create login POST route
     pass
 
 
 @app.route("/")
 def index():
-    #TODO create default route
+    # TODO create default route
     pass
