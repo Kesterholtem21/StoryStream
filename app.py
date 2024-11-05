d3cc4e9c291c6149d9b2b46f10e1da571ae6a4f6
Samuel Beers <101611441+qoietsammy@users.noreply.github.com>
2024-11-05 13:01:25 -0500

    spellcheck

--- app.py
+++ app.py
@@ -44,14 +44,14 @@ class User(db.Model):
     age = db.Column(db.Integer, nullable=False)
 
 
-@app.get("/servey/")
-def get_servey():
+@app.get("/survey/")
+def get_survey():
     # TODO create register GET route
     pass
 
 
-@app.post("/servey/")
-def post_servey():
+@app.post("/survey/")
+def post_survey():
     # TODO create register POST route
     pass
 
