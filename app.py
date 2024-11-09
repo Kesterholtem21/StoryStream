a8eb258f8e259cdc765f54ea782088c12a17fceb
Kesterholtem21 <kesterholtem21@gcc.edu>
2024-11-09 11:15:07 -0500

    Update auth login

--- app.py
+++ app.py
@@ -1,12 +1,27 @@
+###############################################################################
+# Imports
+###############################################################################
+from __future__ import annotations
 from flask import Flask, request, render_template, redirect, url_for, abort, session
 from flask import flash
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin, LoginManager, login_required
+from flask_login import login_user, logout_user, current_user
 from Forms import RegisterForm, LoginForm, PreferenceForm
+from Hasher import UpdatedHasher
 
 # make sure the script's directory is in Python's import path
 # this is only required when run from a different directory
 import os
 import sys
+
+
+###############################################################################
+# Basic Configuration
+###############################################################################
+
+# Identify necessary files
+
 script_dir = os.path.abspath(os.path.dirname(__file__))
 sys.path.append(script_dir)
 # once script directory is in path, import Contact and ContactForm
@@ -14,6 +29,15 @@ sys.path.append(script_dir)
 
 scriptdir = os.path.dirname(os.path.abspath(__file__))
 dbfile = os.path.join(scriptdir, "library.sqlite3")
+pepfile = os.path.join(scriptdir, "pepper.bin")
+
+# open and read the contents of the pepper file into your pepper key
+# NOTE: you should really generate your own and not use the one from the starter
+with open(pepfile, 'rb') as fin:
+  pepper_key = fin.read()
+
+# create a new instance of UpdatedHasher using that pepper key
+pwd_hasher = UpdatedHasher(pepper_key)
 
 # Complete app setup and config
 app = Flask(__name__)
@@ -23,6 +47,23 @@ app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{dbfile}"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+
+
+# Prepare and connect the LoginManager to this app
+login_manager = LoginManager()
+login_manager.init_app(app)
+# function name of the route that has the login form (so it can redirect users)
+login_manager.login_view = 'get_login' # type: ignore
+login_manager.session_protection = "strong"
+# function that takes a user id and
+@login_manager.user_loader
+def load_user(uid: int) -> User|None:
+    return User.query.get(int(uid))
+
+
+###############################################################################
+# Database Setup
+###############################################################################
 
 # Database model for IMDB movies
 
@@ -36,12 +77,31 @@ class Movie(db.Model):
     imdb_rating = db.Column(db.String, nullable=False)
 
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     __tablename__ = 'Users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String, nullable=False)
-    password = db.Column(db.Unicode, nullable=False)
     age = db.Column(db.Integer, nullable=False)
+    passwordHash = db.Column(db.Unicode, nullable=False)
+
+    # make a write-only password property that just updates the stored hash
+    @property
+    def password(self):
+        raise AttributeError("password is a write-only attribute")
+    @password.setter
+    def password(self, pwd: str) -> None:
+        self.passwordHash = pwd_hasher.hash(pwd)
+    
+    # add a verify_password convenience method
+    def verify_password(self, pwd: str) -> bool:
+        return pwd_hasher.check(pwd, self.passwordHash)
+    
+
+
+###############################################################################
+# Route Handlers
+###############################################################################
+
 
 
 @app.get("/viewed/")
@@ -109,38 +169,62 @@ def get_register():
 
 @app.post("/register/")
 def post_register():
-    # TODO create register POST route
-    pass
+    form = RegisterForm()
+    if form.validate():
+        # check if there is already a user with this email address
+        user = User.query.filter_by(username=form.username.data).first()
+        # if the email address is free and passwords match, create a new user and send to login
+        if user is None and form.password.data == form.confirmPassword.data:
+            user = User(email=form.username.data, password=form.password.data) # type:ignore
+            db.session.add(user)
+            db.session.commit()
+            return redirect(url_for('get_login'))
+        else: # if the user already exists
+            # flash a warning message and redirect to get registration form
+            flash('There is already an account with that email address')
+            return redirect(url_for('get_register'))
+    else: # if the form was invalid
+        # flash error messages and redirect to get registration form again
+        for field, error in form.errors.items():
+            flash(f"{field}: {error}")
+        return redirect(url_for('get_register'))
 
 
 @app.get("/login/")
 def get_login():
-    # TODO create login GET route
+    # create login GET route
     form = LoginForm()
     return render_template("login.html", form=form)
 
 
 @app.post("/login/")
 def post_login():
-    # TODO create login POST route
-    # login_form: LoginForm = LoginForm()
-    # if login_form.validate():
-    #     username: str = login_form.username.data
-    #     password: str = login_form.password.data
-    #     if user_db.get(username) == password:
-    #         session['password'] = username
-    #         return redirect(url_for("get_home"))
-    #     else:
-    #         flash("Invalid Username or Password")
-    # else:
-    #     for field, error_msg in login_form.errors.items():
-    #         flash(f"{field}: {error_msg}")
-    # return redirect(url_for("get_login"))
-    return redirect(url_for("get_home"))
+    form = LoginForm()
+    if form.validate():
+        # try to get the user associated with this email address
+        user = User.query.filter_by(email=form.username.data).first()
+        # if this user exists and the password matches
+        if user is not None and user.verify_password(form.password.data):
+            # log this user in through the login_manager
+            login_user(user)
+            # redirect the user to the page they wanted or the home page
+            next = request.args.get('next')
+            if next is None or not next.startswith('/'):
+                next = url_for('index')
+            return redirect(next)
+        else: # if the user does not exist or the password is incorrect
+            # flash an error message and redirect to login form
+            flash('Invalid email address or password')
+            return redirect(url_for('get_login'))
+    else: # if the form was invalid
+        # flash error messages and redirect to get login form again
+        for field, error in form.errors.items():
+            flash(f"{field}: {error}")
+        return redirect(url_for('get_login'))
 
 
 @app.route("/")
 def index():
     # TODO create default route
-    return redirect(url_for("get_login"))
+    return redirect(url_for("get_home"))
     
