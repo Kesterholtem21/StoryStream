0146cbd6bbb8d9e953621a1413c6b6dce3699df1
josephshin22 <shinjj22@gcc.edu>
2024-11-10 23:38:15 -0500

    Update favorites page

--- Forms.py
+++ Forms.py
@@ -1,25 +1,29 @@
 from flask_wtf import FlaskForm
 from wtforms.fields import StringField, IntegerField, SubmitField, SelectField, EmailField, SelectMultipleField
-from wtforms.validators import InputRequired, Optional, Length, Email, NumberRange
+from wtforms.validators import InputRequired, Optional, Length, Email
 
 
 class RegisterForm(FlaskForm):
     # TODO fill this out
     username = StringField("Username", validators=[InputRequired()])
-    password = StringField("Password", validators=[InputRequired(), Length(min=8, max=12)])
-    confirmPassword = StringField("Confirm Password", validators=[InputRequired(), Length(min=8, max=12)])
-    age = IntegerField("Age: ", validators=[InputRequired(),NumberRange(min=0,max=100) ])
+    password = StringField("Password", validators=[
+                           InputRequired(), Length(min=8, max=12)])
+    confirmPassword = StringField("Confirm Password", validators=[
+                                  InputRequired(), Length(min=8, max=12)])
+    age = IntegerField("Age: ", validators=[InputRequired()])
     submit = SubmitField("Register")
 
 
 class LoginForm(FlaskForm):
     # TODO fill this out
     username = StringField("Username", validators=[InputRequired()])
-    password = StringField("Password", validators=[InputRequired(), Length(min=8, max=12)])
+    password = StringField("Password", validators=[
+                           InputRequired(), Length(min=8, max=12)])
     submit = SubmitField("Login")
 
 
 class PreferenceForm(FlaskForm):
     # TODO fill this out
-    genres = SelectMultipleField("Pick Your Favorite Genres: ", validators=[], choices=[""])
-    pass
+    genres = SelectMultipleField(
+        "Pick Your Favorite Genres: ", validators=[], choices=[""])
+    submit = SubmitField("Submit")
